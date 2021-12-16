7a0016457c988d8e568aee43a0f941c69f97695f
Juan Lucas Ormelli de Medeiros <juan_ormelli@hotmail.com>
2021-12-16 11:41:42 -0300

    Ajustes de interface

--- src/components/RegisterModal/index.tsx
+++ src/components/RegisterModal/index.tsx
@@ -24,12 +24,15 @@ export default function RegisterModal(props:RegisterModalProps){
     const [email, setEmail] = useState("")
     const [user, setUser] = useState("")
     const [password, setPassword] = useState("")
+    const [passwordConf, setPasswordConf] = useState("")
+
 
     function openModalClear(){
         setEmail("")
         setPassword("")
         setUser("")
         setMessage("")
+        setPasswordConf('')
     }
     
     async function registerUser({email, password, username}:IRegisterUser) {
@@ -54,9 +57,14 @@ export default function RegisterModal(props:RegisterModalProps){
     
     
     }
+
    
    const [message, setMessage] = useState("")
-   
+   if(passwordConf !== password && message !== "As Senhas sao diferentes"){
+    setMessage("As Senhas sao diferentes")
+    }else if(passwordConf === password && message!==""){
+        setMessage("")
+    }
    
     async function teste () {
         testes = await registerUser({email, password, username:user})
@@ -77,7 +85,36 @@ export default function RegisterModal(props:RegisterModalProps){
     
     }, [message]);
 
-
+    if (message === "As Senhas sao diferentes"){
+        return (
+            <Modal 
+                isOpen={props.isOpen} 
+                onRequestClose={props.onRequestClose}
+                overlayClassName= "react-modal-overlay"
+                className= "react-modal-content"
+                onAfterClose={openModalClear}
+                ariaHideApp={false}
+                >
+    
+                    <div className={style['modal-container']}>
+                        
+                        <button className={style['modal-close']} type="submit" onClick={props.onRequestClose}>✖</button>
+                        <h1 >Cadastrar-se</h1>
+                        <input type="text" value={user} onChange={(event) => {setUser(event.target.value)}} placeholder="Usuario" />
+                        <input type="email" value={email} onChange={(event) => {setEmail(event.target.value)}} placeholder="E-mail" />
+                        <input type="password" value={password} onChange={(event) => {setPassword(event.target.value)}} placeholder="Senha" />
+                        <input type="password" value={passwordConf} onChange={(event) => {setPasswordConf(event.target.value)}} placeholder="Comfirme a Senha" />
+    
+                        <small className={style['error']}>{message}</small>
+                        <button  className={style['loginError']} onClick={()=>{alert("As senhas precisam ser iguais")}}>Cadastrar</button>
+                        
+                    </div>
+                   
+                
+                </Modal>
+            
+        )
+    }else{
     return (
         <Modal 
             isOpen={props.isOpen} 
@@ -95,6 +132,7 @@ export default function RegisterModal(props:RegisterModalProps){
                     <input type="text" value={user} onChange={(event) => {setUser(event.target.value)}} placeholder="Usuario" />
                     <input type="email" value={email} onChange={(event) => {setEmail(event.target.value)}} placeholder="E-mail" />
                     <input type="password" value={password} onChange={(event) => {setPassword(event.target.value)}} placeholder="Senha" />
+                    <input type="password" value={passwordConf} onChange={(event) => {setPasswordConf(event.target.value)}} placeholder="Comfirme a Senha" />
 
                     <small className={style['error']}>{message}</small>
                     <button  className={style['loginError']} onClick={teste}>Cadastrar</button>
@@ -106,6 +144,7 @@ export default function RegisterModal(props:RegisterModalProps){
         
     )
     
+}
 }
 
 
